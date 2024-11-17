538c57a274f3df1d2a11ba517ded5bf382dde802
lasobieralski <dreamer42day@gmail.com>
2024-11-16 18:57:14 -0700

    updated html and css for recipe book.

--- recipes/main.js
+++ recipes/main.js
@@ -1,9 +1,10 @@
-import { recipes } from './recipes.mjs';  // Import the recipes from the recipes.mjs file
+import recipes from './recipes.mjs';  // Import the recipes from the recipes.mjs file
 
 // Example of how to display the recipes on the console
 recipes.forEach(recipe => {
   console.log(`Recipe: ${recipe.name}`);
-  console.log(`Ingredients: ${recipe.ingredients.join(', ')}`);
-  console.log(`Instructions: ${recipe.instructions}`);
+  console.log(`Ingredients: ${recipe.recipeIngredient}`);
+  console.log(`Instructions: ${recipe.recipeInstructions}`);
+  console.log(`Ratings:${recipe.rating}`);
   console.log('----------------------------------');
 });
--- recipes/recipes.mjs
+++ recipes/recipes.mjs
@@ -1,3 +1,4 @@
+
 const recipes = [
 	{
 		author: 'Provo High Culinary Students',
